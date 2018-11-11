f589d78154f0547d211abbc6dd105f92f4d53226
wujunze <itwujunze@163.com>
2018-11-11 16:52:17 +0800

    add ParsePassword function

---
+++ utils/cipher/cipher_test.go
@@ -0,0 +1,59 @@
+package cipher
+
+import (
+	"crypto/rand"
+	"reflect"
+	"testing"
+	pwd "github.com/wujunze/PandaProxy/utils/password"
+)
+
+const (
+	MB = 1024 * 1024
+)
+
+// 测试 cipher 加密解密
+func TestCipher(t *testing.T) {
+	password := pwd.RandPassword()
+	t.Log(password)
+	p, _ := pwd.parsePassword(password)
+	cipher := newCipher(p)
+	// 原数据
+	org := make([]byte, passwordLength)
+	for i := 0; i < passwordLength; i++ {
+		org[i] = byte(i)
+	}
+	// 复制一份原数据到 tmp
+	tmp := make([]byte, passwordLength)
+	copy(tmp, org)
+	t.Log(tmp)
+	// 加密 tmp
+	cipher.encode(tmp)
+	t.Log(tmp)
+	// 解密 tmp
+	cipher.decode(tmp)
+	t.Log(tmp)
+	if !reflect.DeepEqual(org, tmp) {
+		t.Error("解码编码数据后无法还原数据，数据不对应")
+	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+	password := RandPassword()
+	p, _ := parsePassword(password)
+	cipher := newCipher(p)
+	bs := make([]byte, MB)
+	b.ResetTimer()
+	rand.Read(bs)
+	cipher.encode(bs)
+}
+
+func BenchmarkDecode(b *testing.B) {
+	password := RandPassword()
+	p, _ := parsePassword(password)
+	cipher := newCipher(p)
+	bs := make([]byte, MB)
+	b.ResetTimer()
+	rand.Read(bs)
+	cipher.decode(bs)
+}
+
